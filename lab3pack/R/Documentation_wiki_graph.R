b358bb7dfa305247b1b92e687ee31d7fe0c338a8
NI D.W <deaeb@hotmail.com>
2022-09-14 22:29:23 +0200

    Document the dataset using roxygen2:

--- lab3pack/R/Documentation_wiki_graph.R
+++ lab3pack/R/Documentation_wiki_graph.R
@@ -2,16 +2,14 @@
 #' 
 #' a data.frame of a graph with three variables
 #' 
-#' v1, v2: the edges of the graph; w: the weight of the edge
-#' 
+#' @format A data frame with 3 cols and 18 rows:
+#' \describe{
+#'  \item{v1, v2}{the edges of the graph}
+#'  \item{w}{the weight of the edge}
+#' }
 #' @references https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
 #' @seealso https://en.wikipedia.org/wiki/Graph_(mathematics)
 #' 
 
 ## code to prepare `wiki_graph` dataset goes here
-wiki_graph <-
-  data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
-             v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
-             w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
-
-usethis::use_data(wiki_graph, overwrite = TRUE)
+"wiki_graph"
